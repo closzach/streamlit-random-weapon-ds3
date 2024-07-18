6eceb8ee7115e6f0e338fef62cde7971e2eee37e
closzach <constantinloszach@gmail.com>
2024-07-18 10:57:38 +0200

    Affichage des images.

--- 1_Choisir_Arme.py
+++ 1_Choisir_Arme.py
@@ -10,4 +10,4 @@ if gen_arme:
         col1, col2 = st.columns(2)
         arme, lien_img = get_arme_random(get_armes())
         col1.write(arme)
-        col2.markdown(f"<img src=\"{lien_img}\">", unsafe_allow_html=True)
+        col2.image(lien_img, width=150, output_format="PNG")
